67533d74cb8c2604664d8ba32e8660fb776964cf
Fernando Sorto <fernando.sorto.10@gmail.com>
2024-11-17 16:16:22 -0600

    feat(req&res): Adding drf features on requests and responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>', views.snippet_detail),
+    path('snippets2/', views.list_snippets),
+    path('snippets2/<int:pk>/', views.detail_snip),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
